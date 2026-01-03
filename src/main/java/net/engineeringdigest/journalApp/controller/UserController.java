a8c3a32315504f1c6e301c430bb1611f4d5fe134
Prince Borad <pborad112@rku.ac.in>
2026-01-03 19:27:17 +0530

    External API , Value , PostConstruct

--- src/main/java/net/engineeringdigest/journalApp/controller/UserController.java
+++ src/main/java/net/engineeringdigest/journalApp/controller/UserController.java
@@ -1,8 +1,10 @@
 package net.engineeringdigest.journalApp.controller;
 
+import net.engineeringdigest.journalApp.api.response.WeatherResponse;
 import net.engineeringdigest.journalApp.entity.Users;
 import net.engineeringdigest.journalApp.repository.UserRepository;
 import net.engineeringdigest.journalApp.service.UserService;
+import net.engineeringdigest.journalApp.service.WeatherService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,6 +21,9 @@ public class UserController {
 
     @Autowired
     private UserRepository userRepo;
+
+    @Autowired
+    private WeatherService weatherService;
 
     @PutMapping
     public ResponseEntity<?> updateUser(@RequestBody Users user) {
@@ -39,6 +44,22 @@ public class UserController {
         userRepo.deleteByUsername(authentication.getName());
 
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @GetMapping
+    public ResponseEntity<?> greetings() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        WeatherResponse res = weatherService.getWeather("Rajkot");
+
+        String greeting = "";
+
+        if(res != null){
+            greeting += " , Weather Feels Like " + res.getCurrent().getFeelsLike();
+        }
+
+        return new ResponseEntity<>("Hi , " + authentication.getName() + greeting ,
+                HttpStatus.OK);
     }
 
 }
